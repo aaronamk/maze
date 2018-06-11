579da58dd8a550b96fe3566d26103b2844185a03
aaronamk <aaronamk@gmail.com>
2018-06-11 14:51:36 -0400

    Added classes for robot implementation.

---
+++ Maze/test/gui/BasicRobotTest.java
@@ -0,0 +1,14 @@
+package gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BasicRobotTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ Maze/test/gui/RobotDriverTest.java
@@ -0,0 +1,14 @@
+package gui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RobotDriverTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
