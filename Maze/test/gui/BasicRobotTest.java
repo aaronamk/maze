9df5e844443919ec6d3b8baab1de782b29088018
aaronamk <aaronamk@gmail.com>
2018-06-17 22:42:22 -0400

    Added some tests.

--- Maze/test/gui/BasicRobotTest.java
+++ Maze/test/gui/BasicRobotTest.java
@@ -2,13 +2,45 @@ package gui;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import generation.Order;
+import gui.Robot.Direction;
+import gui.Robot.Turn;
+
 class BasicRobotTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	Robot robot;
+	Controller controller;
+	
+	/**
+	 * Creates a robot and connects it to a controller.
+	 */
+	@Before
+	public void setUp() {
+		controller = new Controller();
+		controller.setBuilder(Order.Builder.Prim);
+		controller.start();
+		robot = new BasicRobot();
+		robot.setMaze(controller);
 	}
-
+	
+	/**
+	 * Test case: Tests for sensing, then rotating being consistant.
+	 * <p>
+	 * Correct Outcome: gives the same result no matter which side it is sensing from.
+	 */
+	@Test
+	void testSensingConsistency() {
+		int sense = robot.distanceToObstacle(Direction.LEFT);
+		robot.rotate(Turn.RIGHT);
+		assertEquals(sense, robot.distanceToObstacle(Direction.BACKWARD));
+	}
+	
+	@Test
+	void testOdometer() {
+		int sense = robot.distanceToObstacle(Direction.LEFT);
+		robot.rotate(Turn.RIGHT);
+		assertEquals(sense, robot.distanceToObstacle(Direction.BACKWARD));
+	}
 }
--- Maze/test/gui/RobotDriverTest.java
+++ Maze/test/gui/RobotDriverTest.java
@@ -2,10 +2,17 @@ package gui;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-class RobotDriverTest {
-
+class WallFollowerTest {
+	RobotDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = new WallFollower();
+	}
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
