f48aa9fd9d8a8e72eae58a99a214adb27528dbc2
aaronamk <aaronamk@gmail.com>
2018-06-17 23:06:42 -0400

    BasicRobotTest implemented.

--- Maze/test/gui/BasicRobotTest.java
+++ Maze/test/gui/BasicRobotTest.java
@@ -10,19 +10,37 @@ import gui.Robot.Direction;
 import gui.Robot.Turn;
 
 class BasicRobotTest {
-	Robot robot;
-	Controller controller;
+	Robot robot = new BasicRobot();
+	Controller controller = new Controller();
 	
 	/**
 	 * Creates a robot and connects it to a controller.
 	 */
 	@Before
 	public void setUp() {
-		controller = new Controller();
 		controller.setBuilder(Order.Builder.Prim);
 		controller.start();
-		robot = new BasicRobot();
 		robot.setMaze(controller);
+	}
+	
+	/**
+	 * Test case: Tests for existence of the robot
+	 * <p>
+	 * Correct Outcome: the robot exists
+	 */
+	@Test
+	void testRobotExistence() {
+		assertTrue(robot!=null);
+	}
+	
+	/**
+	 * Test case: Tests for existence of the controller
+	 * <p>
+	 * Correct Outcome: the controller exists
+	 */
+	@Test
+	void testControllerExistence() {
+		assertTrue(controller!=null);
 	}
 	
 	/**
@@ -37,10 +55,28 @@ class BasicRobotTest {
 		assertEquals(sense, robot.distanceToObstacle(Direction.BACKWARD));
 	}
 	
+	/**
+	 * Test case: Tests the odometer
+	 * <p>
+	 * Correct Outcome: the odometer has increased by two
+	 */
 	@Test
 	void testOdometer() {
-		int sense = robot.distanceToObstacle(Direction.LEFT);
-		robot.rotate(Turn.RIGHT);
-		assertEquals(sense, robot.distanceToObstacle(Direction.BACKWARD));
+		robot.move(2, false);
+		assertEquals(2, robot.getOdometerReading());
+	}
+	
+	/**
+	 * Test case: Tests the battery level
+	 * <p>
+	 * Correct Outcome: The battery properly reduces
+	 */
+	@Test
+	void testBattery() {
+		int energy = (int)robot.getBatteryLevel();
+		robot.rotate(Turn.AROUND);
+		robot.rotate(Turn.AROUND);
+		robot.move(1, false);
+		assertEquals(energy, robot.getBatteryLevel()-robot.getEnergyForFullRotation()-robot.getEnergyForStepForward());
 	}
 }
